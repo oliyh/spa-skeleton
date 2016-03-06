6af0d3aeef3a968ab438fc3bee746cdd18235034
Oliver Hine <git@oliy.co.uk>
2016-03-06 17:02:25 +0000

    Moving to pedestal-api

--- src/clj/spa_skeleton/service.clj
+++ src/clj/spa_skeleton/service.clj
@@ -1,14 +1,11 @@
 (ns spa-skeleton.service
   (:require [clojure.core.async :as a]
             [io.pedestal.http :as bootstrap]
-            [io.pedestal.http.body-params :as body-params]
-            [io.pedestal.http.route :as route]
             [io.pedestal.http.route.definition :refer [defroutes]]
             [io.pedestal.impl.interceptor :refer [terminate]]
             [io.pedestal.interceptor.helpers :refer [before handler]]
             [io.pedestal.interceptor :as interceptor]
-            [pedestal.swagger.core :as swagger]
-            [pedestal.swagger.doc :as sw.doc]
+            [pedestal-api.core :as api]
             [ring.util.codec :as codec]
             [ring.util.response :refer [response not-found created resource-response content-type status redirect]]
             [schema.core :as s]
@@ -18,9 +15,10 @@
             [io.pedestal.http.sse :as sse]))
 
 (def status-handler
-  (sw.doc/annotate
+  (api/annotate
    {:summary "Service status"
-    :description "Describes the state of the service"}
+    :description "Describes the state of the service"
+    :parameters {}}
    (handler
     ::status
     (fn [request]
@@ -34,22 +32,24 @@
     (recur)))
 
 (s/with-fn-validation ;; Optional, but nice to have at compile time
-  (swagger/defroutes api-routes
+  (api/defroutes api-routes
     {:info {:title "SPA Skeleton"
             :description "A skeleton project for a ClojureScript Single Page Application backed by a Swagger API"
             :externalDocs {:description "Find out more"
                            :url "https://github.com/oliyh/spa-skeleton"}
             :version "2.0"}}
-    [[["/api" ^:interceptors [(swagger/body-params)
-                              bootstrap/json-body
-                              (swagger/coerce-request)
-                              (swagger/validate-response)]
+    [[["/api" ^:interceptors [api/error-responses
+                              (api/negotiate-response)
+                              (api/body-params)
+                              api/common-body
+                              (api/coerce-request)
+                              (api/validate-response)]
 
        ["/status" {:get status-handler}]
        ["/events" {:get (sse/start-event-stream initialise-stream)}]
 
-       ["/swagger.json" {:get [(swagger/swagger-json)]}]
-       ["/*resource" {:get [(swagger/swagger-ui)]}]]]]))
+       ["/swagger.json" {:get [api/swagger-json]}]
+       ["/*resource" {:get [api/swagger-ui]}]]]]))
 
 (def home
   (handler ::home-handler
